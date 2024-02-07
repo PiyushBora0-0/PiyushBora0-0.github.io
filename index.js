a4781a1b4f2c792ab22250647cb633daae718a09
PiyushBora0-0 <piyushbora10@gmail.com>
2024-02-07 18:28:43 +0530

    added Learning page with link to css formatter and tempereature converter page

---
+++ index.js
@@ -0,0 +1,23 @@
+const textbox = document.getElementById("textbox");
+const toFahrenheight = document.getElementById("toFahrenheight");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+let temp;
+
+
+function convert(){
+    temp = Number(textbox.value);
+
+    if(toFahrenheight.checked){
+        temp= (9/5)*temp+32;
+        result.textContent = `${temp} F`;
+    }
+    else if(toCelcius.checked){
+        temp = (5/9)*(temp-32);
+        result.textContent = `${temp} C`;
+    }
+    else{
+        result.textContent="please enter a unit";
+    }
+    
+}
